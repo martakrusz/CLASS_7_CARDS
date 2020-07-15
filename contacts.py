4727bf2a64a7fb00e17bf84b830190fd7b079b2f
Marta Kruszewska <martakruszewska007@gmail.com>
2020-07-15 22:42:39 +0200

    Fix for loop

--- contacts.py
+++ contacts.py
@@ -1,6 +1,6 @@
 from faker import Faker
 
-class Contacts:
+class Contact:
     def __init__(self, first_name, last_name, company, position, email):
         self.first_name = first_name
         self.last_name = last_name
@@ -10,15 +10,11 @@ class Contacts:
 
 fake = Faker()
 
-person = Contacts(first_name = fake.first_name(), 
+for _ in range(5):
+    person = Contact(first_name = fake.first_name(), 
                     last_name = fake.last_name(), 
                     company = fake.company(), 
-                    position = "Ble", 
+                    position = fake.job(), 
                     email = fake.email())
+    print(person.first_name, person.last_name, person.email, person.position)
 
-
-def make_profiles():
-    for _ in range(5):
-        print(person.first_name, person.last_name, person.email)
-
-make_profiles()
